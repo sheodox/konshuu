836a130e4a1a5c06787a484d3a557aaff822ee92
sheodox <3468630+sheodox@users.noreply.github.com>
2020-06-26 23:24:48 -0500

    automatically scroll to the current day on first load

--- static-src/Day.svelte
+++ static-src/Day.svelte
@@ -15,7 +15,7 @@
     }
 </style>
 
-<div class:today={isToday}>
+<div class:today={isToday} use:scrollToView={isToday}>
     <h2>{day.dayName} {day.date.toLocaleDateString()}</h2>
 	<!-- don't show the work list on the weekend -->
     {#if dayNumber !== 0 && dayNumber !== 6}
@@ -25,12 +25,30 @@
 </div>
 
 <script>
+    import {tick} from 'svelte';
     import TodoList from './TodoList.svelte';
     export let day = {};
     export let dayNumber = 0;
 
     let isToday = false;
+    let dayContainer;
     $: {
         isToday = new Date().toLocaleDateString() === day.date.toLocaleDateString();
     }
+    async function scrollToView(el) {
+    	//if this Day is today, scroll it into view
+		if (isToday) {
+			//the first time this component is rendered we won't be able to scroll any further to the right.
+			//e.x. if today is friday, the first time this function runs the Saturday element won't exist yet,
+			//so scrolling into view now wouldn't center correctly. as it's probably useful to be able to see
+            //the following days we want to wait for a tick to ensure all the days exist and horizontally scrolling
+            //can center this day element
+			await tick();
+			el.scrollIntoView({
+				behavior: 'smooth',
+				block: 'center',
+				inline: 'center'
+			});
+		}
+	}
 </script>
