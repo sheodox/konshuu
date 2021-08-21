860ebe6bbd8562ade3863a23ac80d55e11a24607
sheodox <3468630+sheodox@users.noreply.github.com>
2021-08-21 18:00:55 -0500

    Added a pretty basic home page pitch including an example screenshot.
    Now users who aren't logged in can actually see a bit of what the app
    does instead of just a blank login screen!

--- src/static/landing/Pitch.svelte
+++ src/static/landing/Pitch.svelte
@@ -1,21 +1,88 @@
 <style>
+	.account-form {
+		background: var(--shdx-gray-600);
+		border-radius: 5px;
+		box-shadow: var(--shdx-shadow-3);
+	}
+	section {
+		padding-top: var(--shdx-spacing-10);
+		padding-bottom: var(--shdx-spacing-10);
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	.hero {
+		flex-direction: row;
+		justify-content: center;
+		min-height: 50rem;
+	}
+	.hero-title {
+		font-size: var(--shdx-font-size-11);
+	}
+	.hero-subtitle {
+		font-size: var(--shdx-font-size-8);
+	}
+	.hero > div {
+		max-width: 40rem;
+		padding: var(--shdx-spacing-4);
+	}
+	@media (max-width: 800px) {
+		.hero {
+			flex-direction: column;
+			min-height: unset;
+			padding-top: 0;
+		}
+	}
+	@media (max-width: 600px) {
+		.hero-title {
+			font-size: var(--shdx-font-size-8);
+			text-align: center;
+		}
+		.hero-subtitle {
+			font-size: var(--shdx-font-size-6);
+		}
+	}
+	section:nth-of-type(even) {
+		background: var(--shdx-gray-800);
+	}
+	img {
+		box-shadow: var(--shdx-shadow-5), var(--shdx-shadow-1);
+		border-radius: 15px;
+		max-width: 90vw;
+	}
 </style>
 
 <div id="pitch" class="f-column align-items-center">
-    <h1>Start planning with Konshuu.</h1>
+	<section class="hero">
+		<div>
+			<h1 class="hero-title">Start planning with Konshuu.</h1>
+			<p class="hero-subtitle">Konshuu helps organize your week's daily tasks for work and home.</p>
+		</div>
+		<div class="account-form m-5 p-5 f-column align-items-center">
+			{#if showLogin}
+				<Login />
+				<button on:click={() => (showLogin = false)}>Don't have an account yet?</button>
+			{:else}
+				<SignUp />
+				<button on:click={() => (showLogin = true)}>Already have an account?</button>
+			{/if}
+		</div>
+	</section>
 
-    {#if showLogin}
-        <Login />
-        <button on:click={() => showLogin = false}>Don't have an account yet?</button>
-    {:else}
-        <SignUp />
-        <button on:click={() => showLogin = true}>Already have an account?</button>
-    {/if}
+	<section>
+		<h2>Your Entire Week At A Glance</h2>
+
+		<div>
+			<p>With Konshuu you can see all of your week's tasks at once for your work and personal life.</p>
+		</div>
+		<img src="/landing/example-list.png" alt="example screenshot" />
+	</section>
 </div>
 
 <script>
-    import Login from "./Login.svelte";
-    import SignUp from "./SignUp.svelte";
+	import Login from "./Login.svelte";
+	import SignUp from "./SignUp.svelte";
 
-    let showLogin = true;
+	let showLogin = true;
 </script>
