e87142e3410b09d6772e2334558be38f6177d395
sheodox <3468630+sheodox@users.noreply.github.com>
2021-08-20 22:20:42 -0500

    Show todo menu options on context menu
    This binds to the new sheodox-ui option to show the menu on contextmenu
    on a defined element. Passing in the todo's `li` so it can be opened
    easier when using desktop.

--- src/static/TodoItem.svelte
+++ src/static/TodoItem.svelte
@@ -1,31 +1,37 @@
 <style>
 	li {
-        position: relative;
+		position: relative;
 	}
 	li:hover {
-        background: var(--shdx-gray-400);
-        border-radius: 0.2rem;
+		background: var(--shdx-gray-400);
+		border-radius: 0.2rem;
 	}
-    li :global(div) {
+	li :global(div) {
 		flex: 1;
 		display: flex;
 	}
 	li :global(label) {
-        font-size: 0.8rem;
+		font-size: 0.8rem;
 		flex: 1;
-        padding-left: 0.2rem;
+		padding-left: 0.2rem;
 	}
 	button {
-        padding: 0.3rem;
-        text-align: left;
-    }
+		padding: 0.3rem;
+		text-align: left;
+	}
 	ul {
 		list-style: none;
 	}
 </style>
 
 {#if !todo.completed || !$hideCompleted}
-	<li class="todo-item f-row align-items-start px-1" draggable="true" on:dragstart={dragStart} class:muted={todo.completed}>
+	<li
+		class="todo-item f-row align-items-start px-1"
+		draggable="true"
+		on:dragstart={dragStart}
+		class:muted={todo.completed}
+		bind:this={li}
+	>
 		<Checkbox
 			bind:checked={todo.completed}
 			on:change={() => toggleTodo(todo.id)}
@@ -33,10 +39,10 @@
 		>
 			{todo.text}
 		</Checkbox>
-		<MenuButton triggerClasses="small">
+		<MenuButton triggerClasses="small" contextTriggerElement={li}>
 			<span slot="trigger">
 				<span class="sr-only">Menu</span>
-                <Icon icon="chevron-down" noPadding={true} />
+				<Icon icon="chevron-down" noPadding={true} />
 			</span>
 			<ul slot="menu">
 				<li>
@@ -52,13 +58,16 @@
 					</button>
 				</li>
 				<li>
-					<button class="a" on:click={() => showReschedule = true}>
+					<button class="a" on:click={() => (showReschedule = true)}>
 						<Icon icon="calendar-day" />
 						Reschedule
 					</button>
 				</li>
 				<li>
-					<button class="a" on:click={() => copyToClipboard(todo.text)}>
+					<button
+						class="a"
+						on:click={() => copyToClipboard(todo.text)}
+					>
 						<Icon icon="copy" />
 						Copy
 					</button>
@@ -69,39 +78,45 @@
 {/if}
 
 {#if showReschedule}
-	<Reschedule bind:visible={showReschedule} {listType} {calendarDate} on:reschedule={rescheduleTodo} />
+	<Reschedule
+		bind:visible={showReschedule}
+		{listType}
+		{calendarDate}
+		on:reschedule={rescheduleTodo}
+	/>
 {/if}
 
 <script>
-	import {hideCompleted, updateWeek, copyToClipboard} from "./todosStore";
-	import {Icon, Checkbox, MenuButton} from 'sheodox-ui';
-	import Reschedule from './Reschedule.svelte';
-	import {getRescheduleDestination} from "./reschedule-utils";
+	import { hideCompleted, updateWeek, copyToClipboard } from "./todosStore";
+	import { Icon, Checkbox, MenuButton } from "sheodox-ui";
+	import Reschedule from "./Reschedule.svelte";
+	import { getRescheduleDestination } from "./reschedule-utils";
 
 	export let todo;
 	export let calendarDate;
 	export let listType;
 
-	let showReschedule = false;
+	let showReschedule = false,
+		li;
 
 	async function toggleTodo() {
 		await updateTodo({
-			completed: todo.completed
+			completed: todo.completed,
 		});
 		await updateWeek();
 	}
 
 	async function updateTodo(body) {
 		await fetch(`/todo/${todo.id}`, {
-			method: 'POST',
-			headers: {'Content-Type': 'application/json'},
-			body: JSON.stringify(body)
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(body),
 		});
 	}
 
 	async function removeTodo() {
 		await fetch(`/todo/${todo.id}`, {
-			method: 'DELETE'
+			method: "DELETE",
 		});
 		await updateWeek();
 	}
@@ -110,19 +125,24 @@
 		let newTodo = prompt("Enter a todo", todo.text);
 		if (newTodo && newTodo.trim()) {
 			await updateTodo({
-				text: newTodo
-			})
+				text: newTodo,
+			});
 			await updateWeek();
 		}
 	}
 
 	async function rescheduleTodo(e) {
 		//serialize the date the same way the date input would use for the value
-		await fetch(`/todo/${todo.id}/reschedule/${(getRescheduleDestination(e.detail).serialize())}`);
+		await fetch(
+			`/todo/${todo.id}/reschedule/${getRescheduleDestination(
+				e.detail
+			).serialize()}`
+		);
 		await updateWeek();
 	}
 
 	function dragStart(event) {
-		event.dataTransfer.setData('todoId', todo.id);
+		event.dataTransfer.setData("todoId", todo.id);
 	}
 </script>
+
