71506849501a9b2992c26ea59d83a4426559c123
sheodox <imagamer27@msn.com>
2021-11-19 22:43:31 -0600

    Added a loading indicator when the week of todos hasn't loaded yet.

--- src/static/app/Todos.svelte
+++ src/static/app/Todos.svelte
@@ -5,6 +5,10 @@
 		flex: 1;
 		overflow: auto;
 	}
+	.loading {
+		flex: 1;
+		place-self: center;
+	}
 	@media (max-width: 600px) {
 		.week {
 			scroll-snap-type: y mandatory;
@@ -15,10 +19,15 @@
 <div class="week gap-2 p-1">
 	{#each $week as day}
 		<Day {day} />
+	{:else}
+		<div class="loading">
+			<Loading size="large" />
+		</div>
 	{/each}
 </div>
 
 <script lang="ts">
 	import { week } from './stores/todo';
+	import { Loading } from 'sheodox-ui';
 	import Day from './Day.svelte';
 </script>
