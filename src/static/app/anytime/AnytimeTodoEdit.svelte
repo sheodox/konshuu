72ecb0d5439fed98057e910455e77976d298eb38
sheodox <imagamer27@msn.com>
2022-07-11 19:17:05 -0500

    Put Save/Cancel buttons on the same row
    This takes up a lot of space in a list, it doesn't need to take up so much space.

--- src/static/app/anytime/AnytimeTodoEdit.svelte
+++ src/static/app/anytime/AnytimeTodoEdit.svelte
@@ -1,12 +1,20 @@
+<style>
+	button {
+		margin: 0;
+	}
+</style>
+
 <fieldset>
 	<legend>{title}</legend>
 	<form class="f-column gap-2" on:submit|preventDefault={() => dispatch('submit')}>
 		<TextInput id="todo-text-{id}" bind:value={text}>Text</TextInput>
 		<TextInput id="todo-href-{id}" bind:value={href} placeholder="https://...">URL (optional)</TextInput>
-		<button class="primary" disabled={invalid}>Save</button>
-		{#if showCancel}
-			<button class="secondary" type="button" on:click={() => dispatch('cancel')}>Cancel</button>
-		{/if}
+		<div class="f-row gap-2">
+			{#if showCancel}
+				<button class="secondary f-1" type="button" on:click={() => dispatch('cancel')}>Cancel</button>
+			{/if}
+			<button class="primary f-1" disabled={invalid}>Save</button>
+		</div>
 	</form>
 </fieldset>
 
