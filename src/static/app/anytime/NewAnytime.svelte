e4a1347802ed69e0b025fbfcaa2ed50497b0f335
sheodox <imagamer27@msn.com>
2022-11-05 14:24:28 -0500

    Improve creating a new anytime
    The name text input will now focus automatically, the type defaults to todo list, and it's all wrapped in a form now so you can type a name and press enter to create the anytime.

--- src/static/app/anytime/NewAnytime.svelte
+++ src/static/app/anytime/NewAnytime.svelte
@@ -5,42 +5,44 @@
 </style>
 
 <Modal bind:visible title="New Anytime">
-	<div class="modal-body f-column gap-2">
-		<TextInput id="new-anytime-name" bind:value={name}>Name</TextInput>
-		{#if viewingTag}
-			<div class="mt-2">
-				<Checkbox bind:checked={useCurrentFilters}>Assign tag "{viewingTag.name}"</Checkbox>
-			</div>
-		{/if}
-
-		<div class="f-column gap-2 f-1">
-			<label>
-				Choose a type
-				<br />
-				<select bind:value={kind} class="py-2">
-					{#each anytimeTypes as type}
-						<option value={type.kind}>
-							{type.name}
-						</option>
-					{/each}
-				</select>
-			</label>
-
-			{#if kind === 'countdown'}
-				<CountdownSettings bind:valid={typeSettingsValid} bind:data={kindSpecificData} />
-			{:else if kind === 'countup'}
-				<CountupSettings bind:valid={typeSettingsValid} bind:data={kindSpecificData} />
+	<form on:submit|preventDefault={create}>
+		<div class="modal-body f-column gap-2">
+			<TextInput id="new-anytime-name" bind:value={name}>Name</TextInput>
+			{#if viewingTag}
+				<div class="mt-2">
+					<Checkbox bind:checked={useCurrentFilters}>Assign tag "{viewingTag.name}"</Checkbox>
+				</div>
 			{/if}
+
+			<div class="f-column gap-2 f-1">
+				<label>
+					Choose a type
+					<br />
+					<select bind:value={kind} class="py-2">
+						{#each anytimeTypes as type}
+							<option value={type.kind}>
+								{type.name}
+							</option>
+						{/each}
+					</select>
+				</label>
+
+				{#if kind === 'countdown'}
+					<CountdownSettings bind:valid={typeSettingsValid} bind:data={kindSpecificData} />
+				{:else if kind === 'countup'}
+					<CountupSettings bind:valid={typeSettingsValid} bind:data={kindSpecificData} />
+				{/if}
+			</div>
 		</div>
-	</div>
-	<div class="modal-footer">
-		<button on:click={close} class="secondary">Cancel</button>
-		<button on:click={create} class="primary" disabled={!name || !kind || !typeSettingsValid}>Create</button>
-	</div>
+		<div class="modal-footer">
+			<button on:click={close} class="secondary" type="button">Cancel</button>
+			<button class="primary" disabled={!name || !kind || !typeSettingsValid}>Create</button>
+		</div>
+	</form>
 </Modal>
 
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onMount } from 'svelte';
 	import { Checkbox, TextInput, Modal } from 'sheodox-ui';
 	import { anytimeOps, anytimeTypes, tags } from '../stores/anytime';
 	import { activeRouteParams } from '../stores/routing';
@@ -52,7 +54,7 @@
 	export let visible: boolean;
 
 	let name = '',
-		kind = '',
+		kind = 'todos',
 		useCurrentFilters = true,
 		kindSpecificData: Partial<AnytimeNew> = {},
 		typeSettingsValid = true;
@@ -78,4 +80,8 @@
 		typeSettingsValid = true;
 		kindSpecificData = {};
 	}
+
+	onMount(() => {
+		document.getElementById('text-input-new-anytime-name')?.focus();
+	});
 </script>
